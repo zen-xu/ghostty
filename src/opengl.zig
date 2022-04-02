a54d9bd83b6c4998c7f37a59aa4222754718b2ee
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-04-01 19:33:12 -0700

    comments

--- src/opengl.zig
+++ src/opengl.zig
@@ -1,12 +1,15 @@
 //! OpenGL bindings.
 //!
 //! These are pupose-built for usage within this program. While they closely
-//! align with the OpenGL C APIs, they aren't meant to be general purpose.
-//! Certain use cases will CERTAINLY be sub-optimal by using these helpers
-//! and should use the C API directly.
+//! align with the OpenGL C APIs, they aren't meant to be general purpose,
+//! they aren't meant to have 100% API coverage, and they aren't meant to
+//! be hyper-performant.
 //!
 //! For performance-intensive or unsupported aspects of OpenGL, the C
 //! API is exposed via the `c` constant.
+//!
+//! WARNING: Lots of performance improvements that we can make with Zig
+//! comptime help. I'm deferring this until later but have some fun ideas.
 
 pub const c = @import("opengl/c.zig");
 pub const Buffer = @import("opengl/Buffer.zig");
