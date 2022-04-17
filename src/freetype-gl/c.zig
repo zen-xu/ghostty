d1173626ade8507627401b7e3bd009c305b2eb13
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-04-16 21:18:47 -0700

    remove freetype-gl

--- src/freetype-gl/c.zig
+++
@@ -1,4 +0,0 @@
-pub usingnamespace @cImport({
-    @cInclude("texture-atlas.h");
-    @cInclude("texture-font.h");
-});
--- src/freetype-gl/config.h
+++
@@ -1,1 +0,0 @@
-// Purposely empty! This is just here so that freetype-gl will build.
