d5b018349cd2445c8a539737f20133ab4956b921
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-06-28 10:52:59 -0700

    add bold font, doesn't work yet

--- src/font/font.zig
+++ src/font/font.zig
@@ -4,6 +4,7 @@ pub const Glyph = @import("Glyph.zig");
 
 /// Embedded fonts (for now)
 pub const fontRegular = @import("test.zig").fontRegular;
+pub const fontBold = @import("test.zig").fontBold;
 
 /// The styles that a family can take.
 pub const Style = enum {
--- src/font/test.zig
+++ src/font/test.zig
@@ -1,1 +1,2 @@
 pub const fontRegular = @embedFile("../../fonts/Inconsolata-Regular.ttf");
+pub const fontBold = @embedFile("../../fonts/Inconsolata-Bold.ttf");
