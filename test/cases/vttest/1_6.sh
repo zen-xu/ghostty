aca9077bedb0835924bda6871291c2edc4dfeaae
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-07-22 10:05:01 -0700

    add all vttest screen 1s

---
+++ test/cases/vttest/1_6.sh
@@ -0,0 +1,17 @@
+function test_do {
+  xdotool type "vttest"
+  xdotool key Return
+  sleep 1
+  xdotool type "1"
+  xdotool key Return
+  sleep 0.5
+  xdotool key Return
+  sleep 0.5
+  xdotool key Return
+  sleep 0.5
+  xdotool key Return
+  sleep 0.5
+  xdotool key Return
+  sleep 0.5
+  xdotool key Return
+}
