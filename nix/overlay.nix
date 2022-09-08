d7d372124b54100504f6d30cac1d2084cf998af7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-09-08 13:31:18 -0700

    update to stage3 zig with -fstage1

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -13,8 +13,8 @@ final: prev: rec {
   wraptest = prev.callPackage ./wraptest.nix { };
 
   # zig we want to be the latest nightly since 0.9.0 is not released yet.
-  # NOTE: we are pinned to this master version because it broke at a certain
-  # point due to the self-hosted compiler. We'll fix this ASAP.
-  #zig = final.zigpkgs.master;
-  zig = final.zigpkgs.master-2022-08-19;
+  zig = final.zigpkgs.master;
+
+  # last known working stage1 build, the rest in the future are stage3
+  #zig = final.zigpkgs.master-2022-08-19;
 }
