83c9390ac920c495ec0ad2d5a67ba6d2b9cdccf5
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-10-14 09:57:14 -0700

    pkg/libuv: workaround stage2 issue

--- pkg/libuv/c.zig
+++ pkg/libuv/c.zig
@@ -1,3 +1,11 @@
-pub usingnamespace @cImport({
-    @cInclude("uv.h");
-});
+const builtin = @import("builtin");
+
+pub usingnamespace switch (builtin.zig_backend) {
+    .stage1 => @cImport({
+        @cInclude("uv.h");
+    }),
+
+    // Workaround for:
+    // https://github.com/ziglang/zig/issues/12325
+    else => @import("cimport.zig"),
+};
