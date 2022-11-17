843dc8ae14294c23878f302415b020cf46062108
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-11-17 15:09:53 -0800

    pkg/macos: support CFCharacterSet

--- pkg/macos/foundation.zig
+++ pkg/macos/foundation.zig
@@ -2,6 +2,7 @@ pub const c = @import("foundation/c.zig");
 pub usingnamespace @import("foundation/array.zig");
 pub usingnamespace @import("foundation/attributed_string.zig");
 pub usingnamespace @import("foundation/base.zig");
+pub usingnamespace @import("foundation/character_set.zig");
 pub usingnamespace @import("foundation/data.zig");
 pub usingnamespace @import("foundation/dictionary.zig");
 pub usingnamespace @import("foundation/number.zig");
