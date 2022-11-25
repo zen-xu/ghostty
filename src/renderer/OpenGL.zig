179f5e62833307fb5e38d8c14e8343bac3186d0e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-11-25 13:16:07 -0800

    opengl: continue rendering if single cell has error

--- src/renderer/OpenGL.zig
+++ src/renderer/OpenGL.zig
@@ -769,7 +769,7 @@ pub fn rebuildCells(
         var iter = self.font_shaper.runIterator(self.font_group, row);
         while (try iter.next(self.alloc)) |run| {
             for (try self.font_shaper.shape(run)) |shaper_cell| {
-                assert(try self.updateCell(
+                if (self.updateCell(
                     term_selection,
                     screen,
                     row.getCell(shaper_cell.x),
@@ -777,7 +777,15 @@ pub fn rebuildCells(
                     run,
                     shaper_cell.x,
                     y,
-                ));
+                )) |update| {
+                    assert(update);
+                } else |err| {
+                    log.warn("error building cell, will be invalid x={} y={}, err={}", .{
+                        shaper_cell.x,
+                        y,
+                        err,
+                    });
+                }
             }
         }
 
