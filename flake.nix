a295d1e872e1388619ac9230570860f394418ed9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-08-08 10:32:49 -0700

    revert back to nix 22.11, see comment

--- flake.nix
+++ flake.nix
@@ -2,10 +2,14 @@
   description = "ghostty";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     zig.url = "github:mitchellh/zig-overlay";
+
+    # We want to stay as up to date as possible but need to be careful
+    # that the glibc versions used by our dependencies from Nix are compatible
+    # with the system glibc that the user is building for.
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
 
     # Used for shell.nix
     flake-compat = { url = github:edolstra/flake-compat; flake = false; };
