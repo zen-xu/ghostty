1ded176377c3f4df1e27b023e85e82ea5f0328ec
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-08-10 22:06:16 -0700

    input: only load Keymap on darwin

--- src/input.zig
+++ src/input.zig
@@ -1,12 +1,18 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub usingnamespace @import("input/mouse.zig");
 pub usingnamespace @import("input/key.zig");
 pub const keycodes = @import("input/keycodes.zig");
 pub const Binding = @import("input/Binding.zig");
-pub const Keymap = @import("input/Keymap.zig");
 pub const SplitDirection = Binding.Action.SplitDirection;
 pub const SplitFocusDirection = Binding.Action.SplitFocusDirection;
+
+// Keymap is only available on macOS right now
+pub const Keymap = switch (builtin.os.tag) {
+    .macos => @import("input/Keymap.zig"),
+    else => struct {},
+};
 
 test {
     std.testing.refAllDecls(@This());
