721087be7624ce2fd0def7ff3fba91905072c29e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-08-14 12:40:48 -0700

    macos: send the left/right status of modifier keys

--- macos/Sources/Ghostty/SurfaceView.swift
+++ macos/Sources/Ghostty/SurfaceView.swift
@@ -480,11 +480,20 @@ extension Ghostty {
         
         private static func translateFlags(_ flags: NSEvent.ModifierFlags) -> ghostty_input_mods_e {
             var mods: UInt32 = GHOSTTY_MODS_NONE.rawValue
+            
             if (flags.contains(.shift)) { mods |= GHOSTTY_MODS_SHIFT.rawValue }
             if (flags.contains(.control)) { mods |= GHOSTTY_MODS_CTRL.rawValue }
             if (flags.contains(.option)) { mods |= GHOSTTY_MODS_ALT.rawValue }
             if (flags.contains(.command)) { mods |= GHOSTTY_MODS_SUPER.rawValue }
             if (flags.contains(.capsLock)) { mods |= GHOSTTY_MODS_CAPS.rawValue }
+            
+            // Handle sided input. We can't tell that both are pressed in the
+            // Ghostty structure but thats okay -- we don't use that information.
+            let rawFlags = flags.rawValue
+            if (rawFlags & UInt(NX_DEVICERSHIFTKEYMASK) != 0) { mods |= GHOSTTY_MODS_SHIFT_RIGHT.rawValue }
+            if (rawFlags & UInt(NX_DEVICERCTLKEYMASK) != 0) { mods |= GHOSTTY_MODS_CTRL_RIGHT.rawValue }
+            if (rawFlags & UInt(NX_DEVICERALTKEYMASK) != 0) { mods |= GHOSTTY_MODS_ALT_RIGHT.rawValue }
+            if (rawFlags & UInt(NX_DEVICERCMDKEYMASK) != 0) { mods |= GHOSTTY_MODS_SUPER_RIGHT.rawValue }   
             
             return ghostty_input_mods_e(mods)
         }
