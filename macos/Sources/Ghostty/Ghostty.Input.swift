1fbd5e5590e7413db52b253decbfb98964351004
Gregory Anders <greg@gpanders.com>
2023-09-27 22:30:10 -0500

    macos: add keyEquivalent overload to also return modifier string

--- macos/Sources/Ghostty/Ghostty.Input.swift
+++ macos/Sources/Ghostty/Ghostty.Input.swift
@@ -7,6 +7,28 @@ extension Ghostty {
         return Self.keyToEquivalent[key]
     }
 
+    static func keyEquivalent(key: ghostty_input_key_e, mods: ghostty_input_mods_e) -> String? {
+        guard var key = Self.keyEquivalent(key: key) else { return nil }
+        let flags = Self.eventModifierFlags(mods: mods)
+        if flags.contains(.command) {
+            key = "⌘\(key)"
+        }
+
+        if flags.contains(.shift) {
+            key = "⇧\(key)"
+        }
+
+        if flags.contains(.option) {
+            key = "⌥\(key)"
+        }
+
+        if flags.contains(.control) {
+            key = "⌃\(key)"
+        }
+
+        return key
+    }
+
     /// Returns the event modifier flags set for the Ghostty mods enum.
     static func eventModifierFlags(mods: ghostty_input_mods_e) -> NSEvent.ModifierFlags {
         var flags = NSEvent.ModifierFlags(rawValue: 0);
