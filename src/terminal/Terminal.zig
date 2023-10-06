ec854a20ebef47fcb18393604c116f9b892aa5d0
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-10-06 14:33:02 -0700

    terminal: a lot more index tests

--- src/terminal/Terminal.zig
+++ src/terminal/Terminal.zig
@@ -2828,6 +2828,97 @@ test "Terminal: index from the bottom outside of scroll region" {
     }
 }
 
+test "Terminal: index no scroll region, top of screen" {
+    const alloc = testing.allocator;
+    var t = try init(alloc, 5, 5);
+    defer t.deinit(alloc);
+
+    try t.print('A');
+    try t.index();
+    try t.print('X');
+
+    {
+        var str = try t.plainString(testing.allocator);
+        defer testing.allocator.free(str);
+        try testing.expectEqualStrings("A\n X", str);
+    }
+}
+
+test "Terminal: index bottom of primary screen" {
+    const alloc = testing.allocator;
+    var t = try init(alloc, 5, 5);
+    defer t.deinit(alloc);
+
+    t.setCursorPos(5, 1);
+    try t.print('A');
+    try t.index();
+    try t.print('X');
+
+    {
+        var str = try t.plainString(testing.allocator);
+        defer testing.allocator.free(str);
+        try testing.expectEqualStrings("\n\n\nA\n X", str);
+    }
+}
+
+test "Terminal: index inside scroll region" {
+    const alloc = testing.allocator;
+    var t = try init(alloc, 5, 5);
+    defer t.deinit(alloc);
+
+    t.setScrollingRegion(1, 3);
+    try t.print('A');
+    try t.index();
+    try t.print('X');
+
+    {
+        var str = try t.plainString(testing.allocator);
+        defer testing.allocator.free(str);
+        try testing.expectEqualStrings("A\n X", str);
+    }
+}
+
+test "Terminal: index bottom of scroll region" {
+    const alloc = testing.allocator;
+    var t = try init(alloc, 5, 5);
+    defer t.deinit(alloc);
+
+    t.setScrollingRegion(1, 3);
+    t.setCursorPos(4, 1);
+    try t.print('B');
+    t.setCursorPos(3, 1);
+    try t.print('A');
+    try t.index();
+    try t.print('X');
+
+    {
+        var str = try t.plainString(testing.allocator);
+        defer testing.allocator.free(str);
+        try testing.expectEqualStrings("\nA\n X\nB", str);
+    }
+}
+
+test "Terminal: index bottom of primary screen with scroll region" {
+    const alloc = testing.allocator;
+    var t = try init(alloc, 5, 5);
+    defer t.deinit(alloc);
+
+    t.setScrollingRegion(1, 3);
+    t.setCursorPos(3, 1);
+    try t.print('A');
+    t.setCursorPos(5, 1);
+    try t.index();
+    try t.index();
+    try t.index();
+    try t.print('X');
+
+    {
+        var str = try t.plainString(testing.allocator);
+        defer testing.allocator.free(str);
+        try testing.expectEqualStrings("\n\nA\n\nX", str);
+    }
+}
+
 test "Terminal: DECALN" {
     const alloc = testing.allocator;
     var t = try init(alloc, 2, 2);
