4b1809f35e6bf85073cbda67e6897e8d10183e72
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-10-18 08:15:18 -0700

    add empty Inspector file

---
+++ src/Inspector.zig
@@ -0,0 +1,8 @@
+//! The Inspector is a development tool to debug the terminal. This is
+//! useful for terminal application developers as well as people potentially
+//! debugging issues in Ghostty itself.
+const Inspector = @This();
+
+pub fn init() Inspector {
+    return .{};
+}
