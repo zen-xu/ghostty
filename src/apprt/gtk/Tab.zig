290b79ad621c3db0d8fecc8e7260dc85616a9dbd
Thorsten Ball <mrnugget@gmail.com>
2023-11-01 08:18:01 +0100

    gtk: restore "inherit working directory" behavior

--- src/apprt/gtk/Tab.zig
+++ src/apprt/gtk/Tab.zig
@@ -159,6 +159,7 @@ pub fn newSurface(self: *Tab, parent_: ?*CoreSurface) !*Surface {
         .parent = .{
             .tab = self,
         },
+        .parentSurface = parent_ != null,
         .gl_area = @ptrCast(gl_area),
         .title_label = @ptrCast(self.label_text),
         .font_size = font_size,
