d1de53ed22b33a48a55765310371c659a919efbd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-11-19 22:52:40 -0800

    renderer/opengl: correct shader params

--- src/renderer/shaders/image.f.glsl
+++ src/renderer/shaders/image.f.glsl
@@ -1,11 +1,11 @@
 #version 330 core
 
-in vec2 tex_coords;
+in vec2 tex_coord;
 
 layout(location = 0) out vec4 out_FragColor;
 
 uniform sampler2D image;
 
 void main() {
-    out_FragColor = texture(image, tex_coords);
+    out_FragColor = texture(image, tex_coord);
 }
