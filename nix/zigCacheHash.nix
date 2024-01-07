477a79087b807968ee1312ca49bb28e051d0c8f9
Chris Marchesi <chrism@vancluevertech.com>
2024-01-07 13:51:29 -0800

    nix: update nixpkgs-zig-0-12 input, also cache hash
    This updates zig_0_12 to be built off of 0.12.0-dev.2059+42389cb9c.
    
    Additionally, we update the cache hash.

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-dkMHalbYjq1IGPMnU4v/U0faJH8KpXuQCN6G++WS2n8="
+"sha256-LFwQ1HP+J4CF/MUUG2fYDr3goqxeQ/9SgEkVVrUCvRU="
