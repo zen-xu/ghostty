12f42105585d27e5ab57c99cbb01c68297af83a9
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2024-01-07 20:42:30 +0100

    Update required zig version to 0.12.0-dev.2063+804cee3b9

--- build.zig
+++ build.zig
@@ -20,7 +20,7 @@ const Version = @import("src/build/Version.zig");
 // but we liberally update it. In the future, we'll be more careful about
 // using released versions so that package managers can integrate better.
 comptime {
-    const required_zig = "0.12.0-dev.1754+2a3226453";
+    const required_zig = "0.12.0-dev.2063+804cee3b9";
     const current_zig = builtin.zig_version;
     const min_zig = std.SemanticVersion.parse(required_zig) catch unreachable;
     if (current_zig.order(min_zig) == .lt) {
