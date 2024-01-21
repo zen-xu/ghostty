5622ab370f78f4f60ebb5f09ea18951eaf9e8e3a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-01-21 14:07:16 -0800

    renderer/metal,opengl: premult alpha for fragment shaders for images
    Fixes #1346

--- src/renderer/shaders/image.f.glsl
+++ src/renderer/shaders/image.f.glsl
@@ -7,5 +7,6 @@ layout(location = 0) out vec4 out_FragColor;
 uniform sampler2D image;
 
 void main() {
-    out_FragColor = texture(image, tex_coord);
+    vec4 color = texture(image, tex_coord);
+    out_FragColor = vec4(color.rgb * color.a, color.a);
 }
