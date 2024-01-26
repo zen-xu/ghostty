f98b5b741771ba35c09ccfb1c2247da12b7d517d
Jakub Konka <kubkon@jakubkonka.com>
2024-01-26 12:04:49 +0100

    apple-sdk: use zig's libstd getSdk to get active Apple SDK

--- pkg/apple-sdk/build.zig
+++ pkg/apple-sdk/build.zig
@@ -11,46 +11,19 @@ pub fn build(b: *std.Build) !void {
 /// The module target is used to determine the SDK to use so it must have
 /// a resolved target.
 pub fn addPaths(b: *std.Build, m: *std.Build.Module) !void {
-    // The active SDK we want to use
-    const sdk = try SDK.fromTarget(m.resolved_target.?.result);
-
-    // Get the path to our active Xcode installation. If this fails then
+    // Get the path to our active SDK installation. If this fails then
     // the zig build will fail. We store this in a struct variable so its
     // static and only calculated once per build.
     const Path = struct {
         var value: ?[]const u8 = null;
     };
     const path = Path.value orelse path: {
-        const path = std.mem.trim(u8, b.run(&.{ "xcode-select", "--print-path" }), " \r\n");
+        const path = std.zig.system.darwin.getSdk(b.allocator, m.resolved_target.?.result) orelse "";
         Path.value = path;
         break :path path;
     };
-
-    // Base path
-    const base = b.fmt("{s}/Platforms/{s}.platform/Developer/SDKs/{s}{s}.sdk", .{
-        path,
-        sdk.platform,
-        sdk.platform,
-        sdk.version,
-    });
-
-    m.addSystemFrameworkPath(.{ .cwd_relative = b.pathJoin(&.{ base, "/System/Library/Frameworks" }) });
-    m.addSystemIncludePath(.{ .cwd_relative = b.pathJoin(&.{ base, "/usr/include" }) });
-    m.addLibraryPath(.{ .cwd_relative = b.pathJoin(&.{ base, "/usr/lib" }) });
+    // The active SDK we want to use
+    m.addSystemFrameworkPath(.{ .cwd_relative = b.pathJoin(&.{ path, "/System/Library/Frameworks" }) });
+    m.addSystemIncludePath(.{ .cwd_relative = b.pathJoin(&.{ path, "/usr/include" }) });
+    m.addLibraryPath(.{ .cwd_relative = b.pathJoin(&.{ path, "/usr/lib" }) });
 }
-
-const SDK = struct {
-    platform: []const u8,
-    version: []const u8,
-
-    pub fn fromTarget(target: std.Target) !SDK {
-        return switch (target.os.tag) {
-            .ios => .{ .platform = "iPhoneOS", .version = "" },
-            .macos => .{ .platform = "MacOSX", .version = "14" },
-            else => {
-                std.log.err("unsupported os={}", .{target.os.tag});
-                return error.UnsupportedOS;
-            },
-        };
-    }
-};
