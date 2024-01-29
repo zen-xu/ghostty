375df5715500df9a73c68ccec61c6baa5797186e
Pete Schaffner <peteschaffner@icloud.com>
2024-01-29 21:56:22 +0100

    Tint split view divider based on theme background

--- macos/Sources/Helpers/SplitView/SplitView.Divider.swift
+++ macos/Sources/Helpers/SplitView/SplitView.Divider.swift
@@ -3,6 +3,8 @@ import SwiftUI
 extension SplitView {
     /// The split divider that is rendered and can be used to resize a split view.
     struct Divider: View {
+        @EnvironmentObject var ghostty: Ghostty.App
+
         let direction: SplitViewDirection
         let visibleSize: CGFloat
         let invisibleSize: CGFloat
@@ -42,13 +44,21 @@ extension SplitView {
                 return visibleSize + invisibleSize
             }
         }
-        
+
+        private var color: Color {
+            let backgroundColor = NSColor(ghostty.config.backgroundColor)
+            let isLightBackground = backgroundColor.isLightColor
+            let newColor = isLightBackground ? backgroundColor.shadow(withLevel: 0.1) : backgroundColor.shadow(withLevel: 0.4)
+
+            return Color(nsColor: newColor ?? .gray.withAlphaComponent(0.5))
+        }
+
         var body: some View {
             ZStack {
                 Color.clear
                     .frame(width: invisibleWidth, height: invisibleHeight)
                 Rectangle()
-                    .fill(Color.gray)
+                    .fill(color)
                     .frame(width: visibleWidth, height: visibleHeight)
             }
             .onHover { isHovered in
