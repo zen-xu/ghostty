987c5d213e01774b45790870d325b09a4dcfc76c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-01-29 20:40:05 -0800

    simd: utf8 validation

--- src/simd/aarch64.zig
+++ src/simd/aarch64.zig
@@ -4,11 +4,75 @@
 const std = @import("std");
 const assert = std.debug.assert;
 
+pub inline fn vandq_u8(a: @Vector(16, u8), b: @Vector(16, u8)) @Vector(16, u8) {
+    return asm (
+        \\ and %[ret].16b, %[a].16b, %[b].16b
+        : [ret] "=w" (-> @Vector(16, u8)),
+        : [a] "w" (a),
+          [b] "w" (b),
+    );
+}
+
+pub inline fn vceqq_u8(a: @Vector(16, u8), b: @Vector(16, u8)) @Vector(16, u8) {
+    return asm (
+        \\ cmeq %[ret].16b, %[a].16b, %[b].16b
+        : [ret] "=w" (-> @Vector(16, u8)),
+        : [a] "w" (a),
+          [b] "w" (b),
+    );
+}
+
+pub inline fn vcgeq_u8(a: @Vector(16, u8), b: @Vector(16, u8)) @Vector(16, u8) {
+    return asm (
+        \\ cmhs %[ret].16b, %[a].16b, %[b].16b
+        : [ret] "=w" (-> @Vector(16, u8)),
+        : [a] "w" (a),
+          [b] "w" (b),
+    );
+}
+
+pub inline fn vcgtq_u8(a: @Vector(16, u8), b: @Vector(16, u8)) @Vector(16, u8) {
+    return asm (
+        \\ cmhi %[ret].16b, %[a].16b, %[b].16b
+        : [ret] "=w" (-> @Vector(16, u8)),
+        : [a] "w" (a),
+          [b] "w" (b),
+    );
+}
+
 pub inline fn vdupq_n_u8(v: u8) @Vector(16, u8) {
     return asm (
         \\ dup %[ret].16b, %[value:w]
         : [ret] "=w" (-> @Vector(16, u8)),
         : [value] "r" (v),
+    );
+}
+
+pub inline fn veorq_u8(a: @Vector(16, u8), b: @Vector(16, u8)) @Vector(16, u8) {
+    return asm (
+        \\ eor %[ret].16b, %[a].16b, %[b].16b
+        : [ret] "=w" (-> @Vector(16, u8)),
+        : [a] "w" (a),
+          [b] "w" (b),
+    );
+}
+
+pub inline fn vextq_u8(a: @Vector(16, u8), b: @Vector(16, u8), n: u8) @Vector(16, u8) {
+    assert(n <= 16);
+    return asm (
+        \\ ext %[ret].16b, %[a].16b, %[b].16b, %[n]
+        : [ret] "=w" (-> @Vector(16, u8)),
+        : [a] "w" (a),
+          [b] "w" (b),
+          [n] "I" (n),
+    );
+}
+
+pub inline fn vget_lane_u64(v: @Vector(1, u64)) u64 {
+    return asm (
+        \\ umov %[ret], %[v].d[0]
+        : [ret] "=r" (-> u64),
+        : [v] "w" (v),
     );
 }
 
@@ -20,12 +84,31 @@ pub inline fn vld1q_u8(v: []const u8) @Vector(16, u8) {
     );
 }
 
-pub inline fn vceqq_u8(a: @Vector(16, u8), b: @Vector(16, u8)) @Vector(16, u8) {
+pub inline fn vmaxvq_u8(v: @Vector(16, u8)) u8 {
+    const result = asm (
+        \\ umaxv %[ret:b], %[v].16b
+        : [ret] "=w" (-> @Vector(16, u8)),
+        : [v] "w" (v),
+    );
+
+    return result[0];
+}
+
+pub inline fn vorrq_u8(a: @Vector(16, u8), b: @Vector(16, u8)) @Vector(16, u8) {
     return asm (
-        \\ cmeq %[ret].16b, %[a].16b, %[b].16b
+        \\ orr %[ret].16b, %[a].16b, %[b].16b
         : [ret] "=w" (-> @Vector(16, u8)),
         : [a] "w" (a),
           [b] "w" (b),
+    );
+}
+
+pub inline fn vqtbl1q_u8(t: @Vector(16, u8), idx: @Vector(16, u8)) @Vector(16, u8) {
+    return asm (
+        \\ tbl %[ret].16b, { %[t].16b }, %[idx].16b
+        : [ret] "=w" (-> @Vector(16, u8)),
+        : [idx] "w" (idx),
+          [t] "w" (t),
     );
 }
 
@@ -39,11 +122,13 @@ pub inline fn vshrn_n_u16(a: @Vector(8, u16), n: u4) @Vector(8, u8) {
     );
 }
 
-pub inline fn vget_lane_u64(v: @Vector(1, u64)) u64 {
+pub inline fn vshrq_n_u8(a: @Vector(16, u8), n: u8) @Vector(16, u8) {
+    assert(n <= 8);
     return asm (
-        \\ umov %[ret], %[v].d[0]
-        : [ret] "=r" (-> u64),
-        : [v] "w" (v),
+        \\ ushr %[ret].16b, %[a].16b, %[n]
+        : [ret] "=w" (-> @Vector(16, u8)),
+        : [a] "w" (a),
+          [n] "I" (n),
     );
 }
 
--- src/simd/main.zig
+++ src/simd/main.zig
@@ -1,17 +1,16 @@
 const std = @import("std");
 
 pub const isa = @import("isa.zig");
+pub const utf8 = @import("utf8.zig");
 const index_of = @import("index_of.zig");
 pub usingnamespace index_of;
-
-// const utf8 = @import("utf8.zig");
-// pub usingnamespace utf8;
 
 pub fn main() !void {
     //std.log.warn("ISA={}", .{isa.ISA.detect()});
     const input = "1234567\x1b1234567\x1b";
     //const input = "1234567812345678";
     std.log.warn("result={any}", .{index_of.indexOf(input, 0x1B)});
+    std.log.warn("result={any}", .{utf8.utf8Validate(input)});
 }
 
 test {
