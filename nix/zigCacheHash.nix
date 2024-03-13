61424a875c67bec98ff6a193089420987b85f4b9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-03-13 13:40:42 -0700

    nix: update cache hash

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-kKT4LpedYWOlkZSeJqw4RqwJ5blNqHtoNfHrsG2/sfQ="
+"sha256-Rf0xVSlgIciiZP/ZeAkdHnEAQTWApewNFSY8xSDAMIk="
