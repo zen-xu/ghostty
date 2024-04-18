dbbaceec4a46e32f0c3f6f281a4f5f742e23745e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-04-18 14:48:07 -0700

    fix windows build

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-wre+e9PYK/WWzxjwghIm2YGV9GX1GDU7B3y/6A/QKFA="
+"sha256-HkGZWT8drDMqZOb5tUEmNZ7TpBldM15bAkeNbCh2yT0="
