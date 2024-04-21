2f2d2c3bb123c3217b6b7ad0e9e554125acaa5f3
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-04-21 10:25:18 -0700

    remove nixpkgs fork for zig 0.12

--- flake.nix
+++ flake.nix
@@ -9,16 +9,6 @@
     # system glibc that the user is building for.
     nixpkgs-stable.url = "github:nixos/nixpkgs/release-23.05";
 
-    # This is a nixpkgs mirror (based off of master) that contains
-    # patches for Zig 0.12 (master/nightly).
-    #
-    # This gives an up-to-date Zig that contains the nixpkgs patches,
-    # specifically the ones relating to NativeTargetInfo
-    # (https://github.com/ziglang/zig/issues/15898) in addition to the base
-    # hooks. This is used in the package (i.e. packages.ghostty, not the
-    # devShell) to build a Zig that can be included in a NixOS configuration.
-    nixpkgs-zig-0-12.url = "github:vancluever/nixpkgs/vancluever-zig-0-12";
-
     zig = {
       url = "github:mitchellh/zig-overlay";
       inputs.nixpkgs.follows = "nixpkgs-stable";
@@ -34,7 +24,6 @@
     self,
     nixpkgs-unstable,
     nixpkgs-stable,
-    nixpkgs-zig-0-12,
     zig,
     zls,
     ...
@@ -42,7 +31,6 @@
     builtins.foldl' nixpkgs-stable.lib.recursiveUpdate {} (builtins.map (system: let
       pkgs-stable = nixpkgs-stable.legacyPackages.${system};
       pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
-      pkgs-zig-0-12 = nixpkgs-zig-0-12.legacyPackages.${system};
     in {
       devShell.${system} = pkgs-stable.callPackage ./nix/devShell.nix {
         inherit (pkgs-unstable) tracy;
