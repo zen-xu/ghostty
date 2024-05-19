e710a59a435c81684717919df088385a40a0b30a
Gregory Anders <greg@gpanders.com>
2024-05-19 08:59:57 -0500

    os: log stderr from open command

--- src/os/open.zig
+++ src/os/open.zig
@@ -13,5 +13,24 @@ pub fn open(alloc: Allocator, url: []const u8) !void {
     };
 
     var exe = std.process.Child.init(argv, alloc);
+
+    // Pipe stdout/stderr so we can collect output from the command
+    exe.stdout_behavior = .Pipe;
+    exe.stderr_behavior = .Pipe;
+    var stdout = std.ArrayList(u8).init(alloc);
+    var stderr = std.ArrayList(u8).init(alloc);
+    defer {
+        stdout.deinit();
+        stderr.deinit();
+    }
+
     try exe.spawn();
+
+    // 50 KiB is the default value used by std.process.Child.run
+    try exe.collectOutput(&stdout, &stderr, 50 * 1024);
+
+    _ = try exe.wait();
+    if (stderr.items.len > 0) {
+        std.log.err("os.open: {s}", .{stderr.items});
+    }
 }
