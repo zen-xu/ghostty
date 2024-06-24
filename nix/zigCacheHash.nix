f062d6e4051bb9ee1c89e544f36f9c19a982ec61
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-06-23 21:32:03 -0700

    Update libxev
    Fixes #1836, upstream: https://github.com/mitchellh/libxev/commit/efde8a170836334901ed2d59c98bf832eb48ae3a

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-D4ehUg3T+leY0C0DnGUiXEpHCaFp4nHIzDvtzo8X/Mc="
+"sha256-C+YVTjCuEDLZYZ9/bXfTI2bYVUKUtmxMQsttEcBJQwM="
