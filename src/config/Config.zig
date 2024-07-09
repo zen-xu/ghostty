d97910a4e89151efd169503996162e4a9ada0183
Anton Kastritskii <halloy52@gmail.com>
2024-07-09 10:51:51 +0100

    docs: fix typo

--- src/config/Config.zig
+++ src/config/Config.zig
@@ -668,7 +668,7 @@ keybind: Keybinds = .{},
 ///
 ///   * `true`
 ///   * `false` - windows won't have native decorations, i.e. titlebar and
-///      borders. On MacOS this also disabled tabs and tab overview.
+///      borders. On MacOS this also disables tabs and tab overview.
 
 @"window-decoration": bool = true,
 
