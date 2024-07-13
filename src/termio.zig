ffaf020576e76e67def887a02bb297744c774863
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-07-13 14:29:47 -0700

    termio: move stream handler to dedicated file, remove dep on EventData

--- src/termio.zig
+++ src/termio.zig
@@ -2,6 +2,8 @@
 //! for taking the config, spinning up a child process, and handling IO
 //! with the terminal.
 
+const stream_handler = @import("termio/stream_handler.zig");
+
 pub usingnamespace @import("termio/message.zig");
 pub const reader = @import("termio/reader.zig");
 pub const Options = @import("termio/Options.zig");
@@ -9,6 +11,7 @@ pub const Termio = @import("termio/Termio.zig");
 pub const Thread = @import("termio/Thread.zig");
 pub const DerivedConfig = Termio.DerivedConfig;
 pub const Mailbox = Thread.Mailbox;
+pub const StreamHandler = stream_handler.StreamHandler;
 
 test {
     @import("std").testing.refAllDecls(@This());
