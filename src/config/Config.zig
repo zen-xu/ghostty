b87667c9503a3f9685595b4a6483efcae7f7a848
Jeffrey C. Ollie <jeff@ocjtech.us>
2024-08-02 16:17:10 -0500

    document behavior when no initial window is created and a quit delay is set

--- src/config/Config.zig
+++ src/config/Config.zig
@@ -916,8 +916,11 @@ keybind: Keybinds = .{},
 /// Only implemented on Linux.
 @"quit-after-last-window-closed-delay": ?Duration = null,
 
-/// This controls whether an initial window is created when Ghostty is run. Only
-/// implemented on Linux.
+/// This controls whether an initial window is created when Ghostty
+/// is run. Note that if `quit-after-last-window-closed` is `true` and
+/// `quit-after-last-window-closed-delay` is set, setting `initial-window` to
+/// `false` will mean that Ghostty will quit after the configured delay if no
+/// window is ever created. Only implemented on Linux.
 @"initial-window": bool = true,
 
 /// Whether to enable shell integration auto-injection or not. Shell integration
