097b978052c162af4467f6e4faa55c6973f05cea
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-08-16 16:35:07 -0700

    pkg/macos: animation yeet usingns

--- pkg/macos/animation.zig
+++ pkg/macos/animation.zig
@@ -1,5 +1,7 @@
-pub const c = @import("animation/c.zig");
-pub usingnamespace @import("animation/layer.zig");
+pub const c = @import("animation/c.zig").c;
+
+/// https://developer.apple.com/documentation/quartzcore/calayer/contents_gravity_values?language=objc
+pub extern "c" const kCAGravityTopLeft: *anyopaque;
 
 test {
     @import("std").testing.refAllDecls(@This());
