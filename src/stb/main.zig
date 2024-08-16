3404f8e53a4ed2331496c6892c1a79796259ea0a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-08-16 10:36:10 -0700

    termio: yeet usingns

--- src/stb/main.zig
+++ src/stb/main.zig
@@ -1,4 +1,9 @@
-pub usingnamespace @cImport({
+const c = @cImport({
     @cInclude("stb_image.h");
     @cInclude("stb_image_resize.h");
 });
+
+// We'll just add the exports of the functions or types we actually use
+// here, no need to export everything from the C lib if we don't use it.
+pub const stbi_load_from_memory = c.stbi_load_from_memory;
+pub const stbi_image_free = c.stbi_image_free;
