2f578a0b978d29b6f8fdf194f952f03343d7c0b2
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-08-16 16:33:47 -0700

    pkg/macos: foundation yeet usingns

--- pkg/macos/foundation.zig
+++ pkg/macos/foundation.zig
@@ -1,14 +1,35 @@
-pub const c = @import("foundation/c.zig");
-pub usingnamespace @import("foundation/array.zig");
-pub usingnamespace @import("foundation/attributed_string.zig");
-pub usingnamespace @import("foundation/base.zig");
-pub usingnamespace @import("foundation/character_set.zig");
-pub usingnamespace @import("foundation/data.zig");
-pub usingnamespace @import("foundation/dictionary.zig");
-pub usingnamespace @import("foundation/number.zig");
-pub usingnamespace @import("foundation/string.zig");
-pub usingnamespace @import("foundation/type.zig");
-pub usingnamespace @import("foundation/url.zig");
+const array = @import("foundation/array.zig");
+const attributed_string = @import("foundation/attributed_string.zig");
+const base = @import("foundation/base.zig");
+const character_set = @import("foundation/character_set.zig");
+const data = @import("foundation/data.zig");
+const dictionary = @import("foundation/dictionary.zig");
+const number = @import("foundation/number.zig");
+const string = @import("foundation/string.zig");
+const typepkg = @import("foundation/type.zig");
+const url = @import("foundation/url.zig");
+
+pub const c = @import("foundation/c.zig").c;
+pub const Array = array.Array;
+pub const MutableArray = array.MutableArray;
+pub const AttributedString = attributed_string.AttributedString;
+pub const MutableAttributedString = attributed_string.MutableAttributedString;
+pub const ComparisonResult = base.ComparisonResult;
+pub const Range = base.Range;
+pub const FourCharCode = base.FourCharCode;
+pub const CharacterSet = character_set.CharacterSet;
+pub const Data = data.Data;
+pub const Dictionary = dictionary.Dictionary;
+pub const MutableDictionary = dictionary.MutableDictionary;
+pub const Number = number.Number;
+pub const String = string.String;
+pub const MutableString = string.MutableString;
+pub const StringComparison = string.StringComparison;
+pub const StringEncoding = string.StringEncoding;
+pub const stringGetSurrogatePairForLongCharacter = string.stringGetSurrogatePairForLongCharacter;
+pub const URL = url.URL;
+pub const URLPathStyle = url.URLPathStyle;
+pub const CFRelease = typepkg.CFRelease;
 
 test {
     @import("std").testing.refAllDecls(@This());
