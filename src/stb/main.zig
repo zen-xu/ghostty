d0ca949c098741759d9a99014a17ae53991ada49
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-08-16 10:57:19 -0700

    os: start yeeting

--- src/stb/main.zig
+++ src/stb/main.zig
@@ -7,3 +7,4 @@ const c = @cImport({
 // here, no need to export everything from the C lib if we don't use it.
 pub const stbi_load_from_memory = c.stbi_load_from_memory;
 pub const stbi_image_free = c.stbi_image_free;
+pub const stbir_resize_uint8 = c.stbir_resize_uint8;
