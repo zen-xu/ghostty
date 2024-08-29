f133206c5503645348220eab6dd5e9c00d6abdf6
Mitchell Hashimoto <m@mitchellh.com>
2024-08-28 18:39:52 -0700

    nix: update cache

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-kxLuBjGLtFIP3oFkFfNN+WRwPDBlB/HQjoGzXZsO+M0="
+"sha256-mIUl5j3JxtydoV7ayy3aNrt/jR8+a68lQw6lQimLZEw="
