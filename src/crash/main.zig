4e166246761474c3c524def1c993bd23a0d33fce
Mitchell Hashimoto <m@mitchellh.com>
2024-09-10 21:14:55 -0700

    crash: add directory listing, allocation free

--- src/crash/main.zig
+++ src/crash/main.zig
@@ -2,10 +2,15 @@
 //! whether that's setting up the system to catch crashes (Sentry client),
 //! introspecting crash reports, writing crash reports to disk, etc.
 
+const dir = @import("dir.zig");
 const sentry_envelope = @import("sentry_envelope.zig");
 
 pub const sentry = @import("sentry.zig");
 pub const Envelope = sentry_envelope.Envelope;
+pub const defaultDir = dir.defaultDir;
+pub const Dir = dir.Dir;
+pub const ReportIterator = dir.ReportIterator;
+pub const Report = dir.Report;
 
 // The main init/deinit functions for global state.
 pub const init = sentry.init;
