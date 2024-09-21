578aaa1ba63d31649781386dfde88076a7bfdcde
Mitchell Hashimoto <m@mitchellh.com>
2024-09-20 20:44:48 -0700

    macos: DraggableWindowView helper (unused currently)

--- macos/Sources/Features/Terminal/TerminalView.swift
+++ macos/Sources/Features/Terminal/TerminalView.swift
@@ -83,7 +83,7 @@ struct TerminalView<ViewModel: TerminalViewModel>: View {
                 if (Ghostty.info.mode == GHOSTTY_BUILD_MODE_DEBUG || Ghostty.info.mode == GHOSTTY_BUILD_MODE_RELEASE_SAFE) {
                     DebugBuildWarningView()
                 }
-
+                
                 Ghostty.TerminalSplit(node: $viewModel.surfaceTree)
                     .environmentObject(ghostty)
                     .focused($focused)
