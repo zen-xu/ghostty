8186e95902caf51c8e4276788d9ff7de77080fb0
Jeffrey C. Ollie <jeff@ocjtech.us>
2024-09-23 11:41:39 -0500

    gtk/adw: perform version check at both comptime and runtime

--- src/apprt/gtk/notebook.zig
+++ src/apprt/gtk/notebook.zig
@@ -66,7 +66,7 @@ pub const Notebook = union(enum) {
 
         const tab_view: *c.AdwTabView = c.adw_tab_view_new().?;
 
-        if (comptime adwaita.versionAtLeast(1, 2, 0)) {
+        if (comptime adwaita.versionAtLeast(1, 2, 0) and adwaita.versionAtLeast(1, 2, 0)) {
             // Adwaita enables all of the shortcuts by default.
             // We want to manage keybindings ourselves.
             c.adw_tab_view_remove_shortcuts(tab_view, c.ADW_TAB_VIEW_SHORTCUT_ALL_SHORTCUTS);
