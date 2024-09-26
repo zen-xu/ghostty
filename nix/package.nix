196c9dc3ba39e2042bd3f8498ad948b7267ac874
Jeffrey C. Ollie <jeff@ocjtech.us>
2024-09-26 17:24:16 -0500

    nix: fix up zig hook in package.nix

--- nix/package.nix
+++ nix/package.nix
@@ -34,7 +34,7 @@
   # https://github.com/ziglang/zig/issues/14281#issuecomment-1624220653 is
   # ultimately acted on and has made its way to a nixpkgs implementation, this
   # can probably be removed in favor of that.
-  zig012Hook = zig_0_13.hook.overrideAttrs {
+  zig_hook = zig_0_13.hook.overrideAttrs {
     zig_default_flags = "-Dcpu=baseline -Doptimize=${optimize}";
   };
 
@@ -79,7 +79,7 @@
     name = "ghostty-cache";
     nativeBuildInputs = [
       git
-      zig_0_13.hook
+      zig_hook
     ];
 
     dontConfigure = true;
@@ -117,7 +117,7 @@ in
       ncurses
       pandoc
       pkg-config
-      zig012Hook
+      zig_hook
       wrapGAppsHook4
     ];
 
