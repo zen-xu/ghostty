dc26a9abe0e4b7a4b3688dfc136b96282239a2b0
Mitchell Hashimoto <m@mitchellh.com>
2024-10-15 09:26:43 -0700

    nix hash

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-j8H+xIW7pyk9SqeTJQElbYHDePUuV7Pm0tC4XB8X5cU="
+"sha256-5LBZAExb4PJefW+M0Eo+TcoszhBdIFTGBOv6lte5L0Q="
