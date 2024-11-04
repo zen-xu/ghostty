f8d2ac6c8cd2d75e8c3a4dbeb077d086f6655773
Mitchell Hashimoto <m@mitchellh.com>
2024-11-04 09:51:45 -0800

    update zig-objc
    removes usingnamespace

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-SmBLDC1DqAcccoWoHyBsHKVqNr3sBYW1b+YwAXhFZyY="
+"sha256-dNGDbVaPxDbIrDUkDwjzeRHHVcX4KnWKciXiTp1c7lE="
