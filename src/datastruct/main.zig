3aff43b2e88e0f6cab702de7873e787f06685933
Mitchell Hashimoto <m@mitchellh.com>
2024-11-07 14:57:46 -0800

    datastruct: add intrusive doubly linked list

--- src/datastruct/main.zig
+++ src/datastruct/main.zig
@@ -4,12 +4,14 @@
 const blocking_queue = @import("blocking_queue.zig");
 const cache_table = @import("cache_table.zig");
 const circ_buf = @import("circ_buf.zig");
+const intrusive_linked_list = @import("intrusive_linked_list.zig");
 const segmented_pool = @import("segmented_pool.zig");
 
 pub const lru = @import("lru.zig");
 pub const BlockingQueue = blocking_queue.BlockingQueue;
 pub const CacheTable = cache_table.CacheTable;
 pub const CircBuf = circ_buf.CircBuf;
+pub const IntrusiveDoublyLinkedList = intrusive_linked_list.DoublyLinkedList;
 pub const SegmentedPool = segmented_pool.SegmentedPool;
 
 test {
