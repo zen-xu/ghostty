b26ff0a41b6a11d119135a83328666b18c481a11
Mitchell Hashimoto <m@mitchellh.com>
2024-11-12 19:06:57 -0800

    deps: update zf and nix hash

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-fTNqNTfElvZPxJiNQJ/RxrSMCiKZPU3705CY7fznKhY="
+"sha256-K2BxuRk7B7h0xLvpOQIiKmVfhs7Zj5GjhizCDgRa080="
