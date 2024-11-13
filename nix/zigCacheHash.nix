523e3a6ae3e62463b309c1b3222bafd7b1fb11ef
Mitchell Hashimoto <m@mitchellh.com>
2024-11-12 19:49:45 -0800

    deps: update iterm2 color themes

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-K2BxuRk7B7h0xLvpOQIiKmVfhs7Zj5GjhizCDgRa080="
+"sha256-uQY8KYNPqwVH+LFXXqVGNYZdG2imeCZoamJf7bMokX4="
