037d4364e596cc42aa487d88d08364b5d78a4456
Mitchell Hashimoto <m@mitchellh.com>
2024-11-20 15:28:10 -0800

    config: C API ghostty_config_clone

--- include/ghostty.h
+++ include/ghostty.h
@@ -646,6 +646,7 @@ ghostty_info_s ghostty_info(void);
 
 ghostty_config_t ghostty_config_new();
 void ghostty_config_free(ghostty_config_t);
+ghostty_config_t ghostty_config_clone(ghostty_config_t);
 void ghostty_config_load_cli_args(ghostty_config_t);
 void ghostty_config_load_default_files(ghostty_config_t);
 void ghostty_config_load_recursive_files(ghostty_config_t);
