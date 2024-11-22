a191f3c396eec57293dd5df8a2a56e0b01384f5a
Mitchell Hashimoto <m@mitchellh.com>
2024-11-22 10:43:51 -0800

    apprt: switch to reload_config action that calls update_config API

--- src/apprt/action.zig
+++ src/apprt/action.zig
@@ -201,6 +201,15 @@ pub const Action = union(Key) {
     /// on the app or surface.
     config_change_conditional_state,
 
+    /// A request to reload the configuration. The reload request can be
+    /// from a user or for some internal reason. The reload request may
+    /// request it is a soft reload or a full reload. See the struct for
+    /// more documentation.
+    ///
+    /// The configuration should be passed to updateConfig either at the
+    /// app or surface level depending on the target.
+    reload_config: ReloadConfig,
+
     /// The configuration has changed. The value is a pointer to the new
     /// configuration. The pointer is only valid for the duration of the
     /// action and should not be stored.
@@ -251,6 +260,7 @@ pub const Action = union(Key) {
         key_sequence,
         color_change,
         config_change_conditional_state,
+        reload_config,
         config_change,
     };
 
@@ -512,6 +522,13 @@ pub const ColorKind = enum(c_int) {
 
     // 0+ values indicate a palette index
     _,
+};
+
+pub const ReloadConfig = extern struct {
+    /// A soft reload means that the configuration doesn't need to be
+    /// read off disk, but libghostty needs the full config again so call
+    /// updateConfig with it.
+    soft: bool = false,
 };
 
 pub const ConfigChange = struct {
