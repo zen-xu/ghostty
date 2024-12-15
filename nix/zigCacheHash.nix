854ec586f9e7556764ea80df98083b9390a88a60
mitchellh <1299+mitchellh@users.noreply.github.com>
2024-12-15 01:05:52 +0000

    deps: Update iTerm2 color schemes

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-f95EM0DLe5mmU1bg/IIr6ckhxc2KM1Fh1UAmRpM+V2E="
+"sha256-q9UDVryP50HfeeafgnrOd+D6K+cEy33/05K2TB5qiqw="
