2e048c870c07d8d0df31efbacb4891676216b408
Tristan Partin <tristan@partin.io>
2024-12-28 15:57:01 -0600

    apprt/gtk: style the tab overview when window-theme=ghostty
    This requires libadwaita main (to be 1.8).
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- src/apprt/gtk/App.zig
+++ src/apprt/gtk/App.zig
@@ -990,6 +990,8 @@ fn loadRuntimeCss(
                 \\  --headerbar-fg-color: rgb({d},{d},{d});
                 \\  --headerbar-bg-color: rgb({d},{d},{d});
                 \\  --headerbar-backdrop-color: oklab(from var(--headerbar-bg-color) calc(l * 0.9) a b / alpha);
+                \\  --overview-fg-color: rgb({d},{d},{d});
+                \\  --overview-bg-color: rgb({d},{d},{d});
                 \\  --popover-fg-color: rgb({d},{d},{d});
                 \\  --popover-bg-color: rgb({d},{d},{d});
                 \\}}
@@ -1001,6 +1003,12 @@ fn loadRuntimeCss(
                 \\ background-color: var(--headerbar-backdrop-color);
                 \\}}
             , .{
+                headerbar_foreground.r,
+                headerbar_foreground.g,
+                headerbar_foreground.b,
+                headerbar_background.r,
+                headerbar_background.g,
+                headerbar_background.b,
                 headerbar_foreground.r,
                 headerbar_foreground.g,
                 headerbar_foreground.b,
